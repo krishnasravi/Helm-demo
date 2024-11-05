ab4a975314c3c5554ae4168deaa6d11a0ed8911c
krishnasravi <krishnamohanb30@gmail.com>
2024-11-05 13:50:12 +0530

    Update WelcomeController.java

--- src/main/java/com/example/demo/WelcomeController.java
+++ src/main/java/com/example/demo/WelcomeController.java
@@ -8,6 +8,6 @@ public class WelcomeController {
 
     @GetMapping("/welcome")
     public String welcome() {
-        return "Welcome to Helm demo!";
+        return "Welcome to Helm demo-v1!";
     }
 }
